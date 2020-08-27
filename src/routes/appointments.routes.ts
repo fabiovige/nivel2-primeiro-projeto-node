027783c246a2a56ff936edb99fe513ae1d22d0e5
Fabio Vige <fabiovige@gmail.com>
2020-08-27 19:15:41 -0300

    treinamento nivel 2

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,33 @@
+import { Router } from 'express';
+import { startOfHour, parseISO } from 'date-fns';
+
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+
+const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
+
+// SoC: Separation of Concerns (Separação de Preocupação)
+
+appointmentsRouter.get('/', (request, response) => {
+  const appointments = appointmentsRepository.all();
+
+  return response.json(appointments);
+});
+
+appointmentsRouter.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const parseDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointmentsRepository.findByDate(parseDate);
+
+  if (findAppointmentInSameDate) {
+    return response.status(400).json({ message: 'This appointment is already booked' });
+  }
+
+  const appointment = appointmentsRepository.create(provider, parseDate);
+
+  return response.json(appointment);
+});
+
+export default appointmentsRouter;
