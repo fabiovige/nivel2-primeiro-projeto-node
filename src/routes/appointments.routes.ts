c5c59290d3e7cd8220ee7c15a4b8d12c94e29709
Fabio Vige <fabiovige@gmail.com>
2020-08-28 08:46:12 -0300

    Conceitos de Arquitetura de Software (SOLID)

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { startOfHour, parseISO } from 'date-fns';
-
+import { parseISO } from 'date-fns';
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
 const appointmentsRepository = new AppointmentsRepository();
 
 // SoC: Separation of Concerns (Separação de Preocupação)
+// Rota: Receber a requisição, chamar outro arquivo, devolver resposta
 
 appointmentsRouter.get('/', (request, response) => {
   const appointments = appointmentsRepository.all();
@@ -15,19 +16,24 @@ appointmentsRouter.get('/', (request, response) => {
 });
 
 appointmentsRouter.post('/', (request, response) => {
-  const { provider, date } = request.body;
+  try {
+    const { provider, date } = request.body;
 
-  const parseDate = startOfHour(parseISO(date));
+    const parseDate = parseISO(date);
 
-  const findAppointmentInSameDate = appointmentsRepository.findByDate(parseDate);
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
 
-  if (findAppointmentInSameDate) {
-    return response.status(400).json({ message: 'This appointment is already booked' });
+    const appointment = createAppointment.execute({
+      date: parseDate,
+      provider,
+    });
+
+    return response.json(appointment);
+  } catch (err) {
+    return response.status(400).json({ error: err.message });
   }
-
-  const appointment = appointmentsRepository.create(provider, parseDate);
-
-  return response.json(appointment);
 });
 
 export default appointmentsRouter;
